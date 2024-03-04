647dfe215f102dd7cf45d01e4ff9f5d63d600743
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-04 18:36:27 +0200

    Imported shared module

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedComponent } from './feed/feed.component';
 import { PostsRoutingModule } from './posts-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [FeedComponent],
-  imports: [CommonModule, PostsRoutingModule],
+  imports: [CommonModule, PostsRoutingModule, SharedModule],
 })
 export class PostsModule {}
