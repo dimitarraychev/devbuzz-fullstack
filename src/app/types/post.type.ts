5246fe2f4b20343a1a6bab2300a2f014e97e15f1
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-06 22:10:01 +0200

    Created Post Type

---
+++ src/app/types/post.type.ts
@@ -0,0 +1,11 @@
+export type PostType = {
+  title: string;
+  category: string;
+  description: string;
+  image: string;
+  ownerId: string;
+  ownerName: string;
+  likes: string[];
+  lowercaseTitle: string[];
+  timestamp: string;
+};
