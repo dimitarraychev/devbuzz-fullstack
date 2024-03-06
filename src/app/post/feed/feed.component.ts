811284c03bb2ce552a4e4488fffb4bbc1497f9d2
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-06 22:11:23 +0200

    Implemeneted postService inside Feed Component

--- src/app/post/feed/feed.component.ts
+++ src/app/post/feed/feed.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { PostService } from '../post-service/post.service';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.scss']
+  styleUrls: ['./feed.component.scss'],
 })
-export class FeedComponent {
-
+export class FeedComponent implements OnInit {
+  postService = inject(PostService);
+  ngOnInit(): void {
+    this.postService.getPosts().subscribe((post) => console.log(post));
+  }
 }
