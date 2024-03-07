527473bac4abe5aac58acf3f7068956c05993a87
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-07 10:02:24 +0200

    Renamed PostType

--- src/app/post/post-service/post.service.ts
+++ src/app/post/post-service/post.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { PostType } from 'src/app/types/post.type';
+import { Post } from 'src/app/types/post.type';
 
 @Injectable({
   providedIn: 'root',
@@ -10,9 +10,9 @@ export class PostService {
   firestore: Firestore = inject(Firestore);
   postsCollectionRef = collection(this.firestore, 'posts');
 
-  getPosts(): Observable<PostType[]> {
+  getPosts(): Observable<Post[]> {
     return collectionData(this.postsCollectionRef, {
       idField: 'id',
-    }) as Observable<PostType[]>;
+    }) as Observable<Post[]>;
   }
 }
--- src/app/types/post.type.ts
+++ src/app/types/post.type.ts
@@ -1,11 +1,15 @@
-export type PostType = {
+export type Post = {
   title: string;
   category: string;
   description: string;
   image: string;
-  ownerId: string;
-  ownerName: string;
+  owner: Owner;
   likes: string[];
   lowercaseTitle: string[];
   timestamp: string;
+};
+
+type Owner = {
+  _id: string;
+  name: string;
 };
