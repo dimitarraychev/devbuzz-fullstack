03ccbebd196d2dd78ba2ff4e6bec59c512ac1c4f
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-08 17:17:51 +0200

    Imported Shared Module to User Module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, UserRoutingModule],
+  imports: [CommonModule, SharedModule, UserRoutingModule],
 })
 export class UserModule {}
