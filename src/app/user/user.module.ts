1f13745ac7996f982a4889dca503f4c0870fbdbc
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-11 18:57:27 +0200

    Imported Reactive Forms Module to User Module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,9 +4,10 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, SharedModule, UserRoutingModule],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule, UserRoutingModule],
 })
 export class UserModule {}
