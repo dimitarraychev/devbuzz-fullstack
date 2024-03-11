498a758df612a38c9773e6e23184a8693b346c61
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-11 18:58:23 +0200

    Added check if user's logged in and responsive design

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private userService: UserService) {}
+
   isMenuOpen: boolean = false;
+
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
