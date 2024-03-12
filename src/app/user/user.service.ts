9c65b49dcc84f4b27eb5124ecd46074371f42565
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-12 17:38:19 +0200

    Added setCookie and logout functionalities

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { User } from '../types/user.type';
-import { AuthResponse } from '../types/api.type';
+import { AuthResponse, LogoutResponse } from '../types/api.type';
 import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,11 +14,11 @@ export class UserService {
 
   constructor(private http: HttpClient, private cookieService: CookieService) {}
 
-  get isLogged() {
+  get isLogged(): boolean {
     return !!this.cookieService.get('auth');
   }
 
-  get userInfo() {
+  get userInfo(): { username: string; _id: string } | null {
     if (!this.isLogged) return null;
 
     const info = localStorage.getItem('user');
@@ -26,14 +27,33 @@ export class UserService {
     return JSON.parse(info);
   }
 
-  login(userData: User) {
-    return this.http.post<User>(this.apiUrl + '/users/login', userData);
+  login(userData: User): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(this.apiUrl + '/users/login', userData);
   }
 
-  register(userData: User) {
+  register(userData: User): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(
       this.apiUrl + '/users/register',
       userData
     );
+  }
+
+  setCookieAndStorage(res: AuthResponse): void {
+    this.cookieService.set('auth', res.token, 7);
+    localStorage.setItem(
+      'user',
+      JSON.stringify({ username: res.username, _id: res._id })
+    );
+  }
+
+  logout(): void {
+    this.http.get<LogoutResponse>(this.apiUrl + '/users/logout').subscribe({
+      next: (res) => {
+        this.cookieService.delete('auth');
+        localStorage.removeItem('user');
+      },
+      error: (e) => console.log(e),
+      complete: () => console.info('complete'),
+    });
   }
 }
