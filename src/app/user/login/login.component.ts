5eea65c40803ef825c3eea8256e3153f97ce08b3
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-12 17:37:26 +0200

    Added login component logic

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
   errorMessage: string | null = null;
+  isSubmitted: boolean = false;
+  isButtonDisabled: boolean = false;
+
+  isValid(element: string): boolean | undefined {
+    return (
+      this.loginForm.get(element)?.invalid &&
+      (this.loginForm.get(element)?.dirty ||
+        this.loginForm.get(element)?.touched ||
+        this.isSubmitted)
+    );
+  }
+
+  onSubmit(): void {
+    this.isSubmitted = true;
+
+    const { email, password } = this.loginForm.getRawValue();
+
+    if (this.loginForm.invalid) {
+      this.errorMessage = this.validationErrorHandler();
+      return;
+    }
+
+    this.isButtonDisabled = true;
+
+    this.userService
+      .login({
+        email,
+        password,
+      })
+      .subscribe({
+        next: (res) => this.userService.setCookieAndStorage(res),
+        error: (e) => {
+          this.errorMessage = e.error.message;
+          this.isButtonDisabled = false;
+        },
+        complete: () => this.router.navigate(['/home']),
+      });
+  }
+
+  validationErrorHandler(): string {
+    if (
+      this.loginForm.get('email')?.hasError('required') ||
+      this.loginForm.get('password')?.hasError('required')
+    )
+      return 'Uh-oh! All fields are required.';
+    return 'A wild error occurred! Try again.';
+  }
 }
