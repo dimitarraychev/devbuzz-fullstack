330646073ce7a6a19bda85f0c567c919d901f3d0
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-14 14:19:48 +0200

    Changed routes for cohesiveness

--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -10,12 +10,12 @@ const routes: Routes = [
     component: FeedComponent,
   },
   {
-    path: 'details',
-    component: PostDetailsComponent,
+    path: 'post/create',
+    component: PostCreateComponent,
   },
   {
-    path: 'create',
-    component: PostCreateComponent,
+    path: 'post/:id',
+    component: PostDetailsComponent,
   },
 ];
 
