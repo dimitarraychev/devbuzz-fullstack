0f8073f3d260b86879d85b4a6a3e07657c390874
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-14 14:19:27 +0200

    Details fetches and displays data from API

--- src/app/post/post-details/post-details.component.ts
+++ src/app/post/post-details/post-details.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { Post } from 'src/app/types/post.type';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
   templateUrl: './post-details.component.html',
-  styleUrls: ['./post-details.component.scss']
+  styleUrls: ['./post-details.component.scss'],
 })
-export class PostDetailsComponent {
+export class PostDetailsComponent implements OnInit {
+  constructor(
+    private postService: PostService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
+  post = {} as Post;
+  postId: string = '';
+
+  ngOnInit(): void {
+    this.getPost();
+  }
+
+  getPost(): void {
+    this.postId = this.activatedRoute.snapshot.params['id'];
+
+    this.postService.getPost(this.postId).subscribe({
+      next: (post) => (this.post = post),
+      error: (e) => console.log(e),
+    });
+  }
 }
