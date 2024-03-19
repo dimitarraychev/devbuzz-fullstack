d14a40f37b4b5696053fe17ba28a24bc12b51328
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-19 18:15:50 +0200

    Added routing for Edint Component

--- client/src/app/post/post-routing.module.ts
+++ client/src/app/post/post-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './feed/feed.component';
 import { PostDetailsComponent } from './post-details/post-details.component';
 import { PostCreateComponent } from './post-create/post-create.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'post/:id',
     component: PostDetailsComponent,
+  },
+  {
+    path: 'post/:id/edit',
+    component: PostEditComponent,
   },
 ];
 
