b2c8e1a1a5752d6ef0aebf2815bbae5b01442b31
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-19 18:16:18 +0200

    Added Edit, Delete and Like functionalities

--- client/src/app/post/post.service.ts
+++ client/src/app/post/post.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { NewPost, Post } from '../types/post.type';
 import { Observable } from 'rxjs';
+import { ApiResponse } from '../types/api.type';
 
 @Injectable({
   providedIn: 'root',
@@ -12,19 +13,37 @@ export class PostService {
 
   constructor(private http: HttpClient) {}
 
-  getLatestPosts(): Observable<Post[]> {
+  getLatestPosts$(): Observable<Post[]> {
     return this.http.get<Post[]>(this.apiUrl + '/posts');
   }
 
-  getHottestPosts(): Observable<Post[]> {
+  getHottestPosts$(): Observable<Post[]> {
     return this.http.get<Post[]>(this.apiUrl + '/posts/hottest');
   }
 
-  getPost(postId: string): Observable<Post> {
+  getPost$(postId: string): Observable<Post> {
     return this.http.get<Post>(this.apiUrl + '/posts/' + postId);
   }
 
-  createPost(postData: NewPost): Observable<string> {
-    return this.http.post<string>(this.apiUrl + '/posts', postData);
+  createPost$(postData: NewPost): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(this.apiUrl + '/posts', postData);
+  }
+
+  editPost$(postId: string, postData: NewPost): Observable<ApiResponse> {
+    return this.http.patch<ApiResponse>(
+      this.apiUrl + '/posts/' + postId,
+      postData
+    );
+  }
+
+  deletePost$(postId: string): Observable<ApiResponse> {
+    return this.http.delete<ApiResponse>(this.apiUrl + '/posts/' + postId);
+  }
+
+  likePost$(postId: string): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(
+      this.apiUrl + '/posts/' + postId + '/like',
+      {}
+    );
   }
 }
