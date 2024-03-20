cecf64aaebde42e0e329b88e9f5356ecb704f508
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-20 17:01:59 +0200

    Added a reference to user's created posts

--- server/src/services/postService.js
+++ server/src/services/postService.js
@@ -1,6 +1,13 @@
 const Post = require("../models/Post");
+const User = require("../models/User");
 
-exports.create = (postData) => Post.create(postData);
+exports.create = async (postData) => {
+	const post = await Post.create(postData);
+	await User.findByIdAndUpdate(postData.owner._id, {
+		$addToSet: { posts: post._id },
+	});
+	return post;
+};
 
 exports.getOne = (postId) => Post.findById(postId);
 
