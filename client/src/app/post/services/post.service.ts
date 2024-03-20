48571d768e568582fcedd0a30b5c484b1e10b935
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-20 16:59:54 +0200

    Moved Post Service to services folder

--- client/src/app/post/services/post.service.ts
+++ client/src/app/post/services/post.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { NewPost, Post } from '../types/post.type';
+import { NewPost, Post } from '../../types/post.type';
 import { Observable } from 'rxjs';
-import { ApiResponse } from '../types/api.type';
+import { ApiResponse } from '../../types/api.type';
 
 @Injectable({
   providedIn: 'root',
