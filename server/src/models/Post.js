31ac2685cf98d6f856c1e74827c78a87a676d3a0
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-20 17:01:41 +0200

    Added validation to Post Model in server

--- server/src/models/Post.js
+++ server/src/models/Post.js
@@ -3,40 +3,50 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema(
 	{
 		title: {
-			required: [true, "Title is required"],
+			required: [true, "Title is required!"],
+			minLength: [10, "Title should be between 10 and 100 characters!"],
+			maxLength: [100, "Title should be between 10 and 100 characters!"],
 			type: String,
 		},
 		category: {
-			required: [true, "Category is required"],
+			required: [true, "Category is required!"],
 			type: String,
 		},
 		description: {
-			required: [true, "Description is required"],
+			required: [true, "Description is required!"],
+			minLength: [
+				50,
+				"Description should be between 50 and 3000 characters!",
+			],
+			maxLength: [
+				3000,
+				"Description should be between 50 and 3000 characters!",
+			],
 			type: String,
 		},
 		image: {
 			required: [true, "Image is required"],
 			match: [
 				/^https?:\/\//,
-				"Image should start with http:// or https://",
+				"Image should start with http:// or https://!",
 			],
 			type: String,
 		},
 		owner: {
-			required: [true, "Owner is required"],
+			required: [true, "Owner is required!"],
 			type: {
 				_id: {
-					required: [true, "Owner _id is required"],
+					required: [true, "Owner _id is required!"],
 					type: String,
 				},
 				username: {
-					required: [true, "Owner name is required"],
+					required: [true, "Owner name is required!"],
 					type: String,
 				},
 			},
 		},
 		likes: {
-			required: [true, "Likes is required"],
+			required: [true, "Likes is required!"],
 			type: Array,
 		},
 		comments: [
