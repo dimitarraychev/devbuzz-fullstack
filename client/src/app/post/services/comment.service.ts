c1897c0ec849b3892fe62853c587184aa7c87d27
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-21 17:14:51 +0200

    Added comment service

---
+++ client/src/app/post/services/comment.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponse } from 'src/app/types/api.type';
+import { NewComment } from 'src/app/types/post.type';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentService {
+  apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  createComment$(commentData: NewComment): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(this.apiUrl + '/comments', commentData);
+  }
+
+  // editPost$(postId: string, postData: NewPost): Observable<ApiResponse> {
+  //   return this.http.patch<ApiResponse>(
+  //     this.apiUrl + '/posts/' + postId,
+  //     postData
+  //   );
+  // }
+
+  // deletePost$(postId: string): Observable<ApiResponse> {
+  //   return this.http.delete<ApiResponse>(this.apiUrl + '/posts/' + postId);
+  // }
+}
