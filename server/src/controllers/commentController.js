5c8d22059f44cf80b5e475df5a2860886df4b2f6
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-21 15:40:21 +0200

    Created comment controller on server

---
+++ server/src/controllers/commentController.js
@@ -0,0 +1,86 @@
+const router = require("express").Router();
+const { isAuth } = require("../middlewares/auth");
+const commentService = require("../services/commentService");
+
+const validationRegex = new RegExp(/:\s([A-Z][\w\s]+!)/);
+
+router.post("/", isAuth, async (req, res) => {
+	try {
+		const { username, _id } = req.user;
+
+		const commentData = {
+			...req.body,
+			owner: {
+				_id,
+				username,
+			},
+		};
+
+		const comment = await commentService.add(commentData);
+
+		res.status(201).json({
+			ok: true,
+			message: "Comment successfully added.",
+			_id: comment._id,
+		});
+	} catch (error) {
+		error.message =
+			error.message.match(validationRegex)?.[1] ?? error.message;
+
+		res.status(400).json({
+			ok: false,
+			message: error.message,
+		});
+	}
+});
+
+// router.patch("/:postId", isAuth, async (req, res) => {
+// 	try {
+// 		const postId = req.params.postId;
+// 		const postData = req.body;
+
+// 		const post = await postService.getOne(postId);
+// 		if (post.owner._id != req.user._id)
+// 			throw new Error("Owner verification failed!");
+
+// 		await postService.update(postId, postData);
+
+// 		res.status(200).json({
+// 			ok: true,
+// 			message: "Post successfully edited",
+// 			_id: postId,
+// 		});
+// 	} catch (error) {
+// 		error.message =
+// 			error.message.match(validationRegex)?.[1] ?? error.message;
+
+// 		res.status(400).json({
+// 			ok: false,
+// 			message: error.message,
+// 		});
+// 	}
+// });
+
+// router.delete("/:postId", isAuth, async (req, res) => {
+// 	try {
+// 		const postId = req.params.postId;
+
+// 		const post = await postService.getOne(postId);
+// 		if (post.owner._id != req.user._id)
+// 			throw new Error("Owner verification failed!");
+
+// 		await postService.delete(postId);
+
+// 		res.status(200).json({
+// 			ok: true,
+// 			message: "Post successfully deleted",
+// 		});
+// 	} catch (error) {
+// 		res.status(400).json({
+// 			ok: false,
+// 			message: error.message,
+// 		});
+// 	}
+// });
+
+module.exports = router;
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const authController = require("./controllers/authController");
 const postController = require("./controllers/postController");
+const commentController = require("./controllers/commentController");
 
 router.use("/auth", authController);
 router.use("/posts", postController);
+router.use("/comments", commentController);
 
 module.exports = router;
