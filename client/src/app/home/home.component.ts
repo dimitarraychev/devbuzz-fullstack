1a2a884497fd0d0ef95edef16192045c18cbaf9e
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-22 16:52:19 +0200

    Removed BentoGrid

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,19 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import BentoGrid from '@bentogrid/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  myBento = {} as BentoGrid;
-
-  ngOnInit(): void {
-    this.myBento = new BentoGrid({
-      target: '.bentogrid',
-      columns: 3,
-      cellGap: 10,
-    });
-  }
-}
+export class HomeComponent {}
