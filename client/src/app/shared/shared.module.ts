09a27c026043c560c6f3db60192b862842eca0df
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-22 21:45:23 +0200

    Created components folder in shared module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './button/button.component';
-import { LoaderComponent } from './loader/loader.component';
+import { ButtonComponent } from './components/button/button.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { RelativeTimePipe } from './pipes/relative-time.pipe';
 
 @NgModule({
-  declarations: [ButtonComponent, LoaderComponent],
+  declarations: [ButtonComponent, LoaderComponent, RelativeTimePipe],
   imports: [CommonModule],
-  exports: [ButtonComponent, LoaderComponent],
+  exports: [ButtonComponent, LoaderComponent, RelativeTimePipe],
 })
 export class SharedModule {}
