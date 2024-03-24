dd1c40fdb3dba4e8623add9866700172a44bfba4
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-24 20:10:57 +0200

    Moved cors options to .env file

--- server/src/config/configExpress.js
+++ server/src/config/configExpress.js
@@ -4,15 +4,10 @@ const cookieParser = require("cookie-parser");
 
 const { auth } = require("../middlewares/auth");
 
-const corsOptions = {
-	origin: "http://localhost:4200",
-	credentials: true,
-};
-
 function configExpress(app) {
 	app.use(express.json());
 	app.use(cookieParser());
-	app.use(cors(corsOptions));
+	app.use(cors(process.env.CORS));
 	app.use(auth);
 
 	return app;
