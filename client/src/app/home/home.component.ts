4517e236f4d424491ae18eae5659a5f590a7b15c
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-25 17:45:09 +0200

    Added navigation to feed categories from the home page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,8 +1,29 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  navigateToDevelopment() {
+    this.router.navigate(['/posts', 'feed'], {
+      queryParams: { category: 'development' },
+    });
+  }
+
+  navigateToArtificialIntelligence() {
+    this.router.navigate(['/posts', 'feed'], {
+      queryParams: { category: 'artificial-intelligence' },
+    });
+  }
+
+  navigateToBlockchain() {
+    this.router.navigate(['/posts', 'feed'], {
+      queryParams: { category: 'blockchain' },
+    });
+  }
+}
