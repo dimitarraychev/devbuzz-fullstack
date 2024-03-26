35cca8d302278123c7610ac9bb77c34bccb3f34c
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-26 16:42:07 +0200

    Counting the user's posts before getting them

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -1,17 +1,18 @@
 const User = require("../models/User");
+const Post = require("../models/Post");
 
 exports.getTop = () =>
 	User.find({}, { password: 0, __v: 0 }).sort({ posts: 1 }).limit(3).lean();
 
 exports.getOne = async (userId, limit, skip) => {
+	const totalPosts = await Post.countDocuments({ user: userId });
+
 	const user = await User.findById({ _id: userId }, { password: 0, __v: 0 })
 		.populate({
 			path: "posts",
 			options: { sort: { createdAt: -1 }, limit, skip },
 		})
 		.lean();
-
-	const totalPosts = user.posts.length | 0;
 
 	return { user, totalPosts };
 };
