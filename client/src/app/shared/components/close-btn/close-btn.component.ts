1232314c2b4007eb42335bab706fa5deedbdb5e1
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-28 10:53:15 +0200

    Implemented navigation service in close btn

--- client/src/app/shared/components/close-btn/close-btn.component.ts
+++ client/src/app/shared/components/close-btn/close-btn.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
   selector: 'app-close-btn',
   templateUrl: './close-btn.component.html',
   styleUrls: ['./close-btn.component.scss'],
 })
-export class CloseBtnComponent {}
+export class CloseBtnComponent {
+  constructor(private navigationService: NavigationService) {}
+
+  navigateBack(): void {
+    this.navigationService.back();
+  }
+}
