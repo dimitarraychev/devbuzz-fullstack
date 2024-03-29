61e4d788a65391884318f007b4f945f7f8aaf6c4
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-29 11:25:26 +0200

    Moved Navigation Service to core/services

--- client/src/app/shared/components/close-btn/close-btn.component.ts
+++ client/src/app/shared/components/close-btn/close-btn.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavigationService } from 'src/app/services/navigation.service';
+import { NavigationService } from 'src/app/core/services/navigation.service';
 
 @Component({
   selector: 'app-close-btn',
