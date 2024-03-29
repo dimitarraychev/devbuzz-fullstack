7813752edb5d9a03d4eb8300c606621c80a948c0
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-03-29 10:59:44 +0200

    Added populate so all comment info is returned in server

--- server/src/services/commentService.js
+++ server/src/services/commentService.js
@@ -19,12 +19,12 @@ exports.getOne = (commentId) => Comment.findById(commentId);
 
 exports.delete = async (commentId) => {
 	const comment = await Comment.findByIdAndDelete(commentId);
-	const post = await Post.findById(comment._postId);
+	const post = await Post.findById(comment._postId).populate("comments");
 
-	post.comments = post.comments.filter((id) => id != commentId);
+	post.comments = post.comments.filter((c) => c.id != commentId);
 	await post.save();
 
 	const postObj = post.toObject();
 
-	return post.comments;
+	return postObj.comments;
 };
